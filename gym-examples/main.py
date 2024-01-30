e7f5c5e34cb1fa113be4badb65e86e2c6866bddb
TruongGiangPham-1 <truonggi@ualberta.ca>
2024-01-29 20:40:37 -0700

    added mainfunction to run

---
+++ gym-examples/main.py
@@ -0,0 +1,22 @@
+
+import gymnasium as gym
+import gym_examples
+from gymnasium.wrappers import FlattenObservation
+
+
+env = gym.make('gym_examples/GridWorld-v0', render_mode="human")
+#env = gym.make("LunarLander-v2", render_mode="human")
+env.action_space.seed(42)
+
+wrapped_env = FlattenObservation(env)
+print(wrapped_env.reset())     # E.g.  [3 0 3 3], {}
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
+
