319acc71632e5d4ba82be09d0bf2d0acd00e6bdd
mac-op <htta@ualberta.ca>
2024-02-14 16:01:51 -0700

    Grid World class implementation + refactoring

---
+++ visualization/src/GridWorld.cpp
@@ -0,0 +1,155 @@
+#include "GridWorld.h"
+
+using namespace GWEnv;
+
+void GridWorld::Render() {
+    if (IsCursorHidden()) UpdateCamera(&camera, CAMERA_FREE);
+
+    // Toggle camera controls
+    if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+    {
+        if (IsCursorHidden()) EnableCursor();
+        else DisableCursor();
+    }
+    Action action = agent->Step();
+    switch (action) {
+        case Action::NONE: break;
+        case Action::PLACE:
+            AddBlock(agentPos.x, agentPos.y, agentPos.z, 1);
+            break;
+        case Action::REMOVE:
+            RemoveBlock(agentPos.x, agentPos.y, agentPos.z);
+            break;
+        default:
+            Move(action);
+            break;
+    }
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    BeginMode3D(camera);
+
+        DrawPlanes();
+        DrawBlocks();
+
+    EndMode3D();
+    DrawFPS(10, 10);
+    EndDrawing();
+}
+
+void GridWorld::DrawPlanes() const {
+    float hLength = (float) h * spacing;
+    float wLength = (float) w * spacing;
+    float dLength = (float) d * spacing;
+
+
+    rlBegin(RL_LINES);
+    for (int i = 0; i <= h; i++) {
+        float lineOffset = (float) i * spacing;
+        rlColor3f(0.25f, 0.25f, 1.0f);
+        rlVertex3f(lineOffset, 0.0f, 0.0f);
+        rlVertex3f(lineOffset, wLength, 0.0f);
+
+        rlColor3f(1.0f, 0.25f, 0.25f);
+        rlVertex3f(lineOffset, 0.0f, 0.0f);
+        rlVertex3f(lineOffset, 0.0f, dLength);
+    }
+
+    for (int i = 0; i <= w; i++) {
+        float lineOffset = (float) i * spacing;
+        rlColor3f(0.25f, 0.25f, 1.0f);
+        rlVertex3f(0.0f, lineOffset, 0.0f);
+        rlVertex3f(hLength, lineOffset, 0.0f);
+
+        rlColor3f(0.25f, 1.0f, 0.25f);
+        rlVertex3f(0.0f, lineOffset, 0.0f);
+        rlVertex3f(0.0f, lineOffset, dLength);
+    }
+
+    for (int i = 0; i <= d; i++) {
+        float lineOffset = (float) i * spacing;
+        rlColor3f(1.0f, 0.25f, 0.25f);
+        rlVertex3f(0.0f, 0.0f, lineOffset);
+        rlVertex3f(hLength, 0.0f, lineOffset);
+
+        rlColor3f(0.25f, 1.0f, 0.25f);
+        rlVertex3f(0.0f, 0.0f, lineOffset);
+        rlVertex3f(0.0f, wLength, lineOffset);
+    }
+
+    rlEnd();
+
+}
+
+bool GridWorld::AddBlock(uint32_t x, uint32_t y, uint32_t z, int blockType) {
+    if (x >= h || y >= w || z >= d || grid[x][y][z] != 0 || !QueryPlacement(x, y, z))
+        return false;
+
+    grid[x][y][z] = blockType;
+    return true;
+}
+
+void GridWorld::Move(Action direction) {
+    switch (direction) {
+        case Action::RIGHT:
+            agentPos.x = std::min(h - 1, agentPos.x + 1);
+            break;
+        case Action::LEFT:
+            agentPos.x = std::max(0U, agentPos.x - 1);
+            break;
+        case Action::DOWN:
+            agentPos.y = std::max(0U, agentPos.y - 1);
+            break;
+        case Action::UP:
+            agentPos.y = std::min(w - 1, agentPos.y + 1);
+            break;
+        case Action::FORWARD:
+            agentPos.z = std::min(d - 1, agentPos.z + 1);
+            break;
+        case Action::BACKWARD:
+            agentPos.z = std::max(0U, agentPos.z - 1);
+            break;
+        default:
+            break;
+    }
+}
+
+bool GridWorld::QueryPlacement(uint32_t x, uint32_t y, uint32_t z) const {
+    if (y == 0) return true;
+    if ( (x > 0 && grid[x-1][y][z]) ||
+        (x < h-1 && grid[x+1][y][z]) ) return true;
+
+    else if ( (y > 0 && grid[x][y-1][z]) ||
+        (y < w-1 && grid[x][y+1][z]) ) return true;
+
+    else if ( (z > 0 && grid[x][y][z-1]) ||
+        (z < d-1 && grid[x][y][z+1]) ) return true;
+
+    return false;
+}
+
+void GridWorld::DrawBlocks() const {
+    // Draw every block in grid
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            for (int k = 0; k < d; k++) {
+                if (grid[i][j][k]) {
+                    DrawCube({static_cast<float>(i) * spacing + 0.5f,
+                              static_cast<float>(j) * spacing + 0.5f,
+                              static_cast<float>(k) * spacing + 0.5f},
+                             spacing, spacing, spacing, BLUE);
+                }
+            }
+        }
+    }
+
+    // Draw current agent position
+    DrawCubeWires({static_cast<float>(agentPos.x) * spacing + 0.5f,
+                   static_cast<float>(agentPos.y) * spacing + 0.5f,
+                   static_cast<float>(agentPos.z) * spacing + 0.5f},
+                  spacing, spacing, spacing, RED);
+}
+
+void GridWorld::RemoveBlock(uint32_t x, uint32_t y, uint32_t z) {
+    grid[x][y][z] = 0;
+}
--- visualization/src/main.cpp
+++ visualization/src/main.cpp
@@ -11,163 +11,29 @@
 *
 ********************************************************************************************/
 
-#include <vector>
 #include "raylib.h"
 #include "rlgl.h"
+#include "GridWorld.h"
 
-
-void DrawPlanes(int x, int y, int z, float spacing)
-{
-    float xLength = (float) x * spacing;
-    float yLength = (float) y * spacing;
-    float zLength = (float) z * spacing;
-
-
-    rlBegin(RL_LINES);
-    for (int i = 0; i <= x; i++) {
-        float lineOffset = (float) i * spacing;
-        rlColor3f(0.25f, 0.25f, 1.0f);
-        rlVertex3f(lineOffset, 0.0f, 0.0f);
-        rlVertex3f(lineOffset, yLength, 0.0f);
-
-        rlColor3f(1.0f, 0.25f, 0.25f);
-        rlVertex3f(lineOffset, 0.0f, 0.0f);
-        rlVertex3f(lineOffset, 0.0f, zLength);
-    }
-
-    for (int i = 0; i <= y; i++) {
-        float lineOffset = (float) i * spacing;
-        rlColor3f(0.25f, 0.25f, 1.0f);
-        rlVertex3f(0.0f, lineOffset, 0.0f);
-        rlVertex3f(xLength, lineOffset, 0.0f);
-
-        rlColor3f(0.25f, 1.0f, 0.25f);
-        rlVertex3f(0.0f, lineOffset, 0.0f);
-        rlVertex3f(0.0f, lineOffset, zLength);
-    }
-
-    for (int i = 0; i <= z; i++) {
-        float lineOffset = (float) i * spacing;
-        rlColor3f(1.0f, 0.25f, 0.25f);
-        rlVertex3f(0.0f, 0.0f, lineOffset);
-        rlVertex3f(xLength, 0.0f, lineOffset);
-
-        rlColor3f(0.25f, 1.0f, 0.25f);
-        rlVertex3f(0.0f, 0.0f, lineOffset);
-        rlVertex3f(0.0f, yLength, lineOffset);
-    }
-
-    rlEnd();
-}
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main()
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1000;
+    const int screenHeight = 600;
 
-    std::vector<Vector3> boxes {{0.5f, 0.5f, 0.5f}};
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d picking");
-
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
-    Vector3 cubeSize = { 1.0f, 1.0f, 1.0f };
-
-    Ray ray = { 0 };                    // Picking line ray
-    RayCollision collision = { 0 };     // Ray collision hit info
+    GWEnv::GridWorld env(5, 5, 4, std::make_unique<GWEnv::InteractiveInput>());
+    InitWindow(screenWidth, screenHeight, "Grid World - Interactive input");
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
-    Vector3* adjacentCube = nullptr;
     // Main game loop
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsCursorHidden()) UpdateCamera(&camera, CAMERA_FREE);
-        bool foundAdjCube = false;
-
-
-        // Toggle camera controls
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
-        {
-            if (IsCursorHidden()) EnableCursor();
-            else DisableCursor();
-        }
-
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-        DrawPlanes(5, 5, 4, 1.0f);
-        for (Vector3& cube : boxes) {
-            DrawCube(cube, cubeSize.x, cubeSize.y, cubeSize.z, RED);
-            DrawCubeWires(cube, cubeSize.x, cubeSize.y, cubeSize.z, MAGENTA);
-
-            if (!foundAdjCube) {
-                ray = GetMouseRay(GetMousePosition(), camera);
-                collision = GetRayCollisionBox(ray,
-                                               {{ cube.x - cubeSize.x/2, cube.y - cubeSize.y/2, cube.z - cubeSize.z/2 },
-                                                { cube.x + cubeSize.x/2, cube.y + cubeSize.y/2, cube.z + cubeSize.z/2 }});
-                if (collision.hit) {
-                    foundAdjCube = true;
-                    adjacentCube = &cube;
-                }
-            }
-        }
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            if (foundAdjCube && adjacentCube != nullptr) {
-                Vector3 placement = *adjacentCube;
-                Vector3& collPoint = collision.point;
-                if (collPoint.x == adjacentCube->x + 0.5) {
-                    placement.x = adjacentCube->x + 1;
-                } else if (collPoint.x == adjacentCube->x - 0.5) {
-                    placement.x = adjacentCube->x - 1;
-                } else if (collPoint.y == adjacentCube->y + 0.5) {
-                    placement.y = adjacentCube->y + 1;
-                } else if (collPoint.y == adjacentCube->y - 0.5) {
-                    placement.y = adjacentCube->y - 1;
-                } else if (collPoint.z == adjacentCube->z + 0.5) {
-                    placement.z = adjacentCube->z + 1;
-                } else if (collPoint.z == adjacentCube->z - 0.5) {
-                    placement.z = adjacentCube->z - 1;
-                }
-
-                boxes.push_back(placement);
-            }
-        }
-
-        EndMode3D();
-
-        DrawFPS(10, 10);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        env.Render();
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
