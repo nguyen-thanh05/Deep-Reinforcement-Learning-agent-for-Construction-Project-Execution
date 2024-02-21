4a6e595660a526fa3034fb255cb471e73078f9e3
weiguo <weiguo@ualberta.ca>
2024-02-20 21:00:21 -0700

    add more comprehensive comments

--- MILP.py
+++ MILP.py
@@ -15,11 +15,11 @@ def MILP():
 
 if __name__ == "__main__":
     # leverage as many cores as possible for parallel processing
-    threads = min(36, multiprocessing.cpu_count())
+    threads = multiprocessing.cpu_count()
     print(f'Using {threads} threads')
 
-    # create a toy example first with all parameters hardcoded
-    # TODO: interface with our 3D GridWorld environment
+    # create a toy instance first with all parameters hardcoded
+    # TODO: interface with our 3D GridWorld environment to create test instance
 
     # set the upper bound for number of agents
     maxAgents = 2
@@ -28,14 +28,18 @@ if __name__ == "__main__":
     X = 7
     Y = 7
 
-    # initialize the desired structure, which is specified by a list of
-    # (x,y,z) coordinates. (1, 2, 3) means a block at (1, 2) with height 3.
-    # NOTE: hollow structure is not supported in this implementation
+    # initialize the desired structure, which is specified by a LIST of
+    # (x, y, z) coordinates where (x, y) is the position of the block
+    # looking from the top and z is the height of the structure at that position
+    # (1, 2, 3) means a block at (1, 2) with height 3.
+    # NOTE: hollow structure is not supported based on structure representation
     structure = dict()
     structure[(3, 3)] = 1
     structure[(3, 4)] = 1
 
     # Z = max(all heights in the structure) + 1 is the height of the GridWorld
+    # You can set this to a larger value if you want to but this will increase
+    # the size of the MILP problem.
     Z = max(structure.values()) + 1
 
     MILP()
