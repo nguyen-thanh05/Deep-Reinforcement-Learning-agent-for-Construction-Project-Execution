0088c255b3b7a1cc76a961dad168ac563e920853
mac-op <htta@ualberta.ca>
2024-02-23 19:23:20 -0700

    support for multiple block types

--- visualization/src/GridWorld.cpp
+++ visualization/src/GridWorld.cpp
@@ -18,7 +18,7 @@ void GridWorld::Render() {
     switch (action) {
         case Action::NONE: break;
         case Action::PLACE:
-            AddBlock(agentPos.x, agentPos.y, agentPos.z, 1);
+            AddBlock(agentPos.x, agentPos.y, agentPos.z, currBlockType);
             break;
         case Action::REMOVE:
             RemoveBlock(agentPos.x, agentPos.y, agentPos.z);
@@ -36,10 +36,9 @@ void GridWorld::Render() {
         DrawBlocks();
 
     EndMode3D();
-//    DrawFPS(10, 10);
 
-    DrawRectangle( 10, 10, 240, 130, Fade(SKYBLUE, 0.5f));
-    DrawRectangleLines( 10, 10, 240, 130, BLUE);
+    DrawRectangle( 10, 10, 240, 150, Fade(SKYBLUE, 0.5f));
+    DrawRectangleLines( 10, 10, 240, 150, BLUE);
 
     DrawText(IsCursorHidden() ? "Mode: Free camera - right click to switch"
         : "Mode: Fixed Camera - right click to switch", 20, 20, 10, BLACK);
@@ -48,6 +47,11 @@ void GridWorld::Render() {
     DrawText("X: Delete block", 40, 80, 10, DARKGRAY);
     DrawText("SPACE: Place block", 40, 100, 10, DARKGRAY);
     DrawText("ENTER: Save to file", 40, 120, 10, DARKGRAY);
+
+    DrawRectangle( 900, 10, 70, 70, Fade(blockColors[currBlockType], 0.5f));
+    DrawRectangleLines( 900, 10, 70, 70, blockColors[currBlockType]);
+    DrawText("Block type", 910, 20, 10, DARKGRAY);
+    DrawText(std::to_string(currBlockType).c_str(), 930, 35, 20, DARKGRAY);
 
     EndDrawing();
 }
@@ -96,7 +100,7 @@ void GridWorld::DrawPlanes() const {
 }
 
 bool GridWorld::AddBlock(int x, int y, int z, int blockType) {
-    if (x >= h || y >= w || z >= d || grid[x][y][z] != 0 || !QueryPlacement(x, y, z))
+    if (x >= h || y >= w || z >= d || grid[x][y][z] == blockType || !QueryPlacement(x, y, z))
         return false;
 
     grid[x][y][z] = blockType;
@@ -153,8 +157,10 @@ void GridWorld::DrawBlocks() const {
                     float posZ = static_cast<float>(k) * spacing + 0.5f;
 
                     DrawCube({posX, posY, posZ},
-                             spacing, spacing, spacing, BLUE);
-                    DrawCubeWires({posX, posY, posZ}, spacing, spacing, spacing, RED);
+                             spacing, spacing, spacing,
+                             Fade(blockColors[grid[i][j][k]], 0.5f));
+                    DrawCubeWires({posX, posY, posZ},
+                                  spacing, spacing, spacing, RED);
                 }
             }
         }
@@ -221,6 +227,14 @@ Action GridWorld::Step() {
     // Do not allow save file if nothing changes
     static bool enterDelayed = false;
 
+    // C-style enums lmao :) hope raylib doesn't change this.
+    for (int i = 1; i <= nBlockTypes; i++) {
+        if (IsKeyPressed(i + KEY_ZERO)) {
+            currBlockType = i;
+            return Action::NONE;
+        }
+    }
+
     if (IsKeyPressed(KEY_ENTER)) {
         if (enterDelayed) return Action::NONE;
         enterDelayed = true;
@@ -229,9 +243,6 @@ Action GridWorld::Step() {
         return Action::NONE;
     }
 
-    // A little hack to avoid setting enterDelayed = true in every if statement
-    bool prevDelayedStatus = enterDelayed;
-    enterDelayed = false;
     if (IsCursorHidden()) return Action::NONE;
 
     if (IsKeyPressed(KEY_W)) return Action::FORWARD;
@@ -241,10 +252,15 @@ Action GridWorld::Step() {
     if (IsKeyPressed(KEY_E)) return Action::UP;
     if (IsKeyPressed(KEY_Q)) return Action::DOWN;
 
-    if (IsKeyPressed(KEY_SPACE)) return Action::PLACE;
-    if (IsKeyPressed(KEY_X)) return Action::REMOVE;
+    if (IsKeyPressed(KEY_SPACE)) {
+        enterDelayed = false;
+        return Action::PLACE;
+    }
+    if (IsKeyPressed(KEY_X)) {
+        enterDelayed = false;
+        return Action::REMOVE;
+    }
 
-    enterDelayed = prevDelayedStatus;
     return Action::NONE;
 }
 
--- visualization/src/main.cpp
+++ visualization/src/main.cpp
@@ -4,20 +4,32 @@
 #include "GridWorld.h"
 
 void PrintError(){
-    std::cerr << "Usage: ./GridWorldEnv <env_size> <s|l> <path>\n\n"
-              << "<env_size>: size of the environment\n"
-              << "<s|l>     : whether to save to file or load from file\n"
-              << "<path>    : path to file to load from / save to\n";
+    std::cerr << "Usage: ./GridWorldEnv <env_size> <num_block_types> <s|l> <file_name>\n\n"
+              << "<env_size>       : size of the environment\n"
+              << "<num_block_types>: number of block types\n"
+              << "<s|l>            : whether to save to file or load from file\n"
+              << "<file_name>      : path to file to load from / save to\n";
     exit(1);
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc != 4) PrintError();
+    for (int i =0; i < argc; i++) {
+        std::cout << argv[i] << " ";
+    }
+    std::cout << argc << std::endl;
+
+    if (argc != 5) PrintError();
 
     int size = std::stoi(argv[1]);
+    int blockTypes = std::stoi(argv[2]);
 
-    std::string operation{argv[2]};
+    if (blockTypes > 13) {
+        std::cerr << "Support for more than 13 block types not supported yet";
+        exit(1);
+    }
+
+    std::string operation{argv[3]};
     if (operation != "s" && operation != "l") PrintError();
     bool save = operation == "s";
 
@@ -32,12 +44,12 @@ int main(int argc, char* argv[])
         }
     }
 
-    fs::path filePath = modelsPath / argv[3];
+    fs::path filePath = modelsPath / argv[4];
 
     const int screenWidth = 1000;
     const int screenHeight = 600;
 
-    GWEnv::GridWorld env(size, size, size, filePath.generic_string(), save);
+    GWEnv::GridWorld env(size, size, size, blockTypes, filePath.generic_string(), save);
     InitWindow(screenWidth, screenHeight, "Grid World");
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
