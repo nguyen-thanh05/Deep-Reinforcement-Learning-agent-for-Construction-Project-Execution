7cf47c2bedb16c96ea87bb82e56a5af729ad699d
weiguo <weiguo@ualberta.ca>
2024-02-26 18:48:51 -0700

    define function signature and make calls

--- MILP.py
+++ MILP.py
@@ -9,7 +9,7 @@
 import multiprocessing
 
 
-def MILP():
+def MILP(maxAgents, X, Y, Z, structure, objective, time_limit, threads, sol_height, sol_paths, sol_pickup, sol_delivery):
     pass
 
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # TODO: interface with our 3D GridWorld environment to create test instance
 
     # set the upper bound for number of agents
-    maxAgents = 2
+    max_agents = 2
 
     # set the length and width of the grid
     X = 7
@@ -42,4 +42,23 @@ if __name__ == "__main__":
     # the size of the MILP problem.
     Z = max(structure.values()) + 1
 
-    MILP()
+    # initialize parameters for the MILP solver
+    status = 'Unknown'
+    run_time = 0.0
+    sum_of_costs = -1
+    sol_paths = {}
+    sol_pickup = {}
+    sol_delivery = {}
+    sol_height = {}
+    time_limit = 60.0
+
+    # prioritize the makespan over the sum of costs
+    for makespan in range(3, 10000):
+
+        # solve the MILP problem
+        iter_T = makespan + 1
+        output = MILP(max_agents, iter_T, X, Y, Z, structure, 'Sum-of-costs', time_limit, threads,
+                      sol_height, sol_paths, sol_pickup, sol_delivery)
+        (status, iter_run_time) = output[:2]
+        run_time += iter_run_time
+        time_limit -= iter_run_time
